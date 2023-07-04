90a649dd2836ab327fb807a8be768600b73ee065
Arteks718 <artexbro.official@gmail.com>
2023-07-04 17:02:17 +0300

    Add button change theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <button className="absolute left-12 top-10 block bg-azure py-5 px-7 text-white text-xl rounded-full">Change Theme</button>
+    </>
   );
 }
 
